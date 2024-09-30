9991770dd513030c40d629d4fb57d59aac389edd
JMS <john.melvin@nextuple.com>
2024-09-30 15:38:48 -0400

    Pass currentTimeSeconds to DemoController

--- app/src/main/java/com/nextuple/nsf/retrofit/api/DemoApi.kt
+++ app/src/main/java/com/nextuple/nsf/retrofit/api/DemoApi.kt
@@ -6,20 +6,23 @@ import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
+import java.util.concurrent.TimeUnit
 
 interface DemoApi {
 
-	@POST("/v1/demo/bopis/{option}")
+	@POST("v1/demo/bopis/{option}")
 	suspend fun createFRBOPIS(
 		@Path("option") option: Int,
 		@Query("store") store: String,
+		@Query("currentTimeSeconds") currentTimeSeconds: Long = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()),
 		@Header("userId") userId: String,
 	): ApiResponse<List<DemoCreateFRResponse?>>
 
-	@POST("/v1/demo/sdd/{option}")
+	@POST("v1/demo/sdd/{option}")
 	suspend fun createFRSDD(
 		@Path("option") option: Int,
 		@Query("store") store: String,
+		@Query("currentTimeSeconds") currentTimeSeconds: Long = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()),
 		@Header("userId") userId: String,
 	): ApiResponse<List<DemoCreateFRResponse?>>
 }
